783c84814044134f6f7645d6b0eae7de9d7be41d
GamerPixelDev <agamerpixel@gmail.com>
2025-11-19 17:49:34 +0100

    Añadir descripción en cada ejercicio

--- bibliotecaMisteriosa.py
+++ bibliotecaMisteriosa.py
@@ -1,3 +1,15 @@
+#Ejercicio 1 – “La biblioteca misteriosa” (Python)
+#Tienes una lista de diccionarios que representan libros. Cada libro tiene:
+# -título
+# -autor
+# -año
+# -número de páginas
+#El objetivo es escribir una función que:
+#Reciba esta lista.
+#Devuelva solo los libros publicados después del año 2000.
+#Ordenados de mayor a menor número de páginas.
+#Y que el resultado final sea una lista de los títulos, no de los diccionarios completos.
+
 libros = [
     {"titulo": "El viaje del norte", "autor": "M. Ruiz", "anio": 1998, "paginas": 320},
     {"titulo": "Sombras de acero", "autor": "L. Bronte", "anio": 2005, "paginas": 410},
