e8e4caaed4aa378f6bd2711cea3810bb68a574a8
GamerPixelDev <agamerpixel@gmail.com>
2025-11-20 12:47:23 +0100

    Ejercicio 11 con otro filtro

--- ejercicio11.py
+++ ejercicio11.py
@@ -26,3 +26,27 @@ def vehiculos_seleccionados(listaVehiculos):
     return [f"{r['modelo']} ({r['año']}) - {r['km']}km" for r in ordenados]
 
 print(vehiculos_seleccionados(vehiculos))
+
+#Ejercicio 11B – El criba-locura de vehículos
+#Tienes la misma lista de vehículos, pero ahora la función se llama
+#vehiculos_premium(listaVehiculos) y debe cumplir TODAS estas condiciones a la vez:
+#    Solo queremos vehículos NO vendidos.
+#    Año entre 2016 y 2020, ambos incluidos.
+#    Menos de 120.000 km, pero OJO:
+#    Si el vehículo tiene más de 80.000 km, entonces solo pasa el filtro si el año es 2019 o 2020. (Esto es la trampa lógica típica)
+#    Debes ordenarlos primero por año descendente (más nuevos primero) y si empatan, por km ascendente (menos km primero).
+#    Devuelve strings con formato:
+#        "MODELO – AÑO – X km"
+
+def vehiculos_premium(listaVehiculos):
+    nVendidos = [nv for nv in listaVehiculos
+                    if not nv["vendido"]
+                    and 2016 <= nv["año"] <= 2020
+                    and ((nv["km"] <= 80000)
+                            or (nv["km"] > 80000 and nv["año"] >= 2019)
+                        )
+                ]
+    ordenados = sorted(nVendidos, key=lambda o: (o["año"], o["km"]))
+    return [f"{r['modelo']} - {r['año']} - {r['km']}km" for r in ordenados]
+
+print(vehiculos_premium(vehiculos))
