7928d1e3592ea45d80ea0e6fb665ef7143b580a8
GamerPixelDev <agamerpixel@gmail.com>
2025-11-20 13:48:09 +0100

    Ejercicio 12B completo

--- ejercicio12.py
+++ ejercicio12.py
@@ -26,3 +26,33 @@ def envios_prioritarios(listaEnvios):
     return [f"ID {r['id']} -> {r['peso']}kg {'(Urgente)' if r['urgente'] else ''}" for r in ordenados]
 
 print(envios_prioritarios(envios))
+
+#Ejercicio 12B — “La clasificación imposible… hasta que la ves”
+#Partimos de la misma lista de envíos, pero ahora la función se llama envios_clasificados y cambia el juego.
+#Nuevas reglas infernales:
+#   Filtra los envíos según estas normas:
+#       Entran todos los urgentes.
+#       Entran los que vayan a Madrid si pesan más de 1.5 kg.
+#       Entran los que pesen más de 10 kg vaya donde vayan, aunque no sean urgentes.
+#   Una vez filtrados, ordénalos así:
+#       Primero los urgentes, ordenados por peso descendente.
+#       Después los no urgentes, también por peso descendente.
+#   Y aquí viene la salsa:
+#       Devuelve una lista de strings con este formato:
+#           [URGENTE] ID 104 → 12.0kg → Sevilla
+#           [NORMAL]  ID 105 → 3.3kg → Madrid
+#       El prefijo va entre corchetes.
+#       El destino debe ir siempre al final.
+#   Para rematar:
+#       Si el envío pesa más de 8kg, añade al final: "⚠️ PESADO" (sin paréntesis, a pelo)
+
+def envios_clasificados(listaEnvios):
+    enviosUrgentes = [eu for eu in listaEnvios
+                        if eu["urgente"]
+                            or (eu["destino"] == "Madrid" and eu["peso"] > 1.5)
+                            or eu["peso"] > 10
+                    ]
+    ordenados = sorted (enviosUrgentes, key=lambda o: (o["urgente"] and o["peso"], not o["urgente"] and o["peso"]), reverse=True)
+    return [f"{'[URGENTE]' if r['urgente'] else '[NORMAL]'} ID {r['id']} -> {r['peso']}kg -> {r['destino']} {'⚠️ PESADO' if r['peso'] > 8 else ''}" for r in ordenados]
+
+print(envios_clasificados(envios))
